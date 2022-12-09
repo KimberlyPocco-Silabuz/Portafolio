7716f873f3360fe707ac66a4bb04d8f9f63f8cc4
KimberlyPocco <kimberly.poccop@gmail.com>
2022-12-09 01:10:05 -0500

    modificado railway static

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATICFIELDS_DIR=(os.path.join(BASE_DIR,'static'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -128,7 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFIELDS_DIR=(os.path.join(BASE_DIR,'static'))
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
